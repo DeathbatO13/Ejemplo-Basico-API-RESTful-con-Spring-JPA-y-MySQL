d6f5220477e2071e669891c202755228b2caa675
DeathbatO13 <danielalejo99@gmail.com>
2025-10-06 12:12:17 -0500

    Clase mapeada en MySQL

---
+++ src/main/java/com/deathbato/apirest_productos/Producto.java
@@ -0,0 +1,75 @@
+package com.deathbato.apirest_productos;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String detalle;
+    private BigDecimal precio;
+    @CreationTimestamp
+    private LocalDateTime fechaCreada;
+    @UpdateTimestamp
+    private LocalDateTime fechaActualizado;
+
+    public Producto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public LocalDateTime getFechaCreada() {
+        return fechaCreada;
+    }
+
+    public void setFechaCreada(LocalDateTime fechaCreada) {
+        this.fechaCreada = fechaCreada;
+    }
+
+    public LocalDateTime getFechaActualizado() {
+        return fechaActualizado;
+    }
+
+    public void setFechaActualizado(LocalDateTime fechaActualizado) {
+        this.fechaActualizado = fechaActualizado;
+    }
+}
